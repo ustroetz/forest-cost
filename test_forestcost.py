4a9ec3c94e66170eca7d1ec8e7973bbb1fefe6b6
Ulrich Stroetz <ustroetz@gmail.com>
2014-01-06 15:18:40 -0800

    add road model

--- test_forestcost.py
+++ test_forestcost.py
@@ -2,30 +2,32 @@ from forestcost import main_model as m
 from forestcost import routing as r
 from forestcost import landing
 from forestcost import gis
+from forestcost import RoadModel as rm
 from pprint import pprint
 import ogr
 
 if __name__ == '__main__':
+    
     ### GIS Data
-    slope_raster = 'testdata//Slope.tif'
-    elevation_raster = 'testdata//Slope.tif'
-    
+    slope_raster = 'testdata/Slope.tif'
+    elevation_raster = 'testdata/Slope.tif'
+    costSurface = 'testdata/CostSurface.tif'
+    newRoadsfn = 'testdata/newRoad.shp'  # in EPSG 3857
     standfn = 'testdata/test_stands.shp' # in EPSG 3857
-    stand_shp = ogr.Open(standfn)
-    stand_lyr = stand_shp.GetLayer()
+    mill_shp = 'testdata/mills.shp'  # in EPSG 3857
+      
+    # Create new road shapefile
+    rm.main(standfn,costSurface,newRoadsfn)
     
-    roadfn = 'testdata/newRoad2.shp' # in EPSG 3857
-    
-    ### Mill information
-    mill_shp = 'testdata/mills.shp'
-    #mill_coords = (-122.665014,45.519218)
-
     # Road Landing Coordinates
-    coords_landing_road = landing.road(roadfn,standfn)
+    coords_landing_road = landing.road(newRoadsfn,standfn)
     
     # Routing
     haulDist, haulTime, coord_mill = r.routing(coords_landing_road,mill_coords=None, mill_shp=mill_shp)
+    
         
+    stand_shp = ogr.Open(standfn)
+    stand_lyr = stand_shp.GetLayer()
     for stand in stand_lyr:
                 
         stand_geom = stand.GetGeometryRef()
@@ -35,26 +37,21 @@ if __name__ == '__main__':
         elevation = gis.zonal_stats(elevation_raster, standfn, stand_wkt)
         slope = gis.zonal_stats(slope_raster, standfn, stand_wkt)
         
-        # stand landing coords
-        coords_landing_stand = landing.stand(stand_wkt,roadfn)        
+        coords_landing_stand = landing.stand(stand_wkt,newRoadsfn)        
 
         ### Tree Data ###
         # Harvest Type (clear cut = 0, partial cut = 1)
         PartialCut = 0
-        
         # Hardwood Fraction
         HdwdFractionCT = 0.15
         HdwdFractionSLT = 0.0
         HdwdFractionLLT = 0.0
-        
         # Chip Trees
         RemovalsCT = 200.0
         TreeVolCT = 5.0
-        
         # Small Log Trees
         RemovalsSLT = 100.00
         TreeVolSLT = 70.0
-        
         # Large Log Trees
         RemovalsLLT = 20.00
         TreeVolLLT = 100.00
@@ -81,8 +78,7 @@ if __name__ == '__main__':
             coords_landing_road,
             haulDist,
             haulTime,
-            coord_mill
-        )
+            coord_mill)
         
         pprint(cost)
 
